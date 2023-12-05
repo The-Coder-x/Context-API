637ec9fac0b011138c1e0d730903bc1dd76ccb99
Rohit <therohitsingh82@gmail.com>
2023-12-06 00:47:34 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import {
-  ChakraProvider,
- 
-  theme,
-} from '@chakra-ui/react';
-
-
+import { ChakraProvider, theme } from '@chakra-ui/react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Home from './components/Home';
+import Cart from './components/Cart';
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      Rohit
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" Component={Home} exact />
+          <Route path="/cart" Component={Cart} />
+        </Routes>
+      </BrowserRouter>
     </ChakraProvider>
   );
 }
