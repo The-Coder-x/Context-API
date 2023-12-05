80f96399820b2444631e70877843645a16058b53
Rohit <therohitsingh82@gmail.com>
2023-12-06 01:00:32 +0530

    header finish

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../style.css';
+import {ColorModeSwitcher} from '../ColorModeSwitcher';
 
 const Header = () => {
   return (
-    <div>
-      HEader
+    <div className = 'header'>
+      <div className="box1">
+        <span className="heading">React Context API</span>
+        <ColorModeSwitcher justifySelf="flex-end" />
+      </div>
+      <ul className="nav">
+        <li className="pod1">
+          <Link to={'/'}>Home</Link>
+        </li>
+        <li className="pod2">
+          <Link to={'/cart'}>Cart</Link>
+        </li>
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
