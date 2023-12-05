a5028b451f755000c7a630bcc031e5686fe9de28
Rohit <therohitsingh82@gmail.com>
2023-12-06 01:30:55 +0530

    Cart finish

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import { useContext, useEffect, useState } from 'react';
+import { Cxt } from '../Context';
+import SingleProduct from './SingleProduct';
 
 const Cart = () => {
-  return (
-    <div>
-      Cart
-    </div>
-  )
-}
+  const { cart } = useContext(Cxt);
+  const [total, setTotal] = useState();
 
-export default Cart
+  useEffect(() => {
+    setTotal(cart.reduce((acc, curr) => acc + Number(curr.price), 0));
+  }, [cart]);
+
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <span style={{ fontSize: 30 }}>My Cart</span>
+      <br />
+      <span style={{ fontSize: 30 }}>Total: {total}</span>
+      <div className="productContainer">
+        {cart.map(prod => (
+          <SingleProduct prod={prod} key={prod.id} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
